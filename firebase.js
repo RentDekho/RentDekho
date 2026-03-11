54ca40af96d13607d58fc697feb7c9323ddb2943
RentDekho <rentdekho.in@gmail.com>
2026-03-12 00:52:13 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth, RecaptchaVerifier } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "rentdekho-90245.firebaseapp.com",
+  projectId: "rentdekho-90245",
+  storageBucket: "rentdekho-90245.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth, RecaptchaVerifier };
